72de717f2481eaa39088f2bafa785f059eb9013c
DaneKellogg <dane.smith@kellogg.northwestern.edu>
2022-03-15 00:31:44 +0000

    save4

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,27 @@ class SessionsController < ApplicationController
   end
 
   def create
+    entered_email = params["email"]
+    entered_password = params["password"]
+    @user = User.find_by({email: entered_email})
+    if @user
+      if BCrypt::Password.new(@user.password) == entered_password
+        session["user_id"] = @user.id
+        redirect_to "/places"
+      else
+        flash[:notice] = "Incorrect Login"
+        redirect_to "/sessions/new"
+      end
+    else
+      flash[:notice] = "Incorrect Login"
+      redirect_to "/sessions/new"
+    end
   end
 
   def destroy
+    session["user_id"] = nil
+    flash[:notice] = "Logged Out!" 
+    redirect_to "/sessions/new"
   end
 end
   
